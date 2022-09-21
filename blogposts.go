691ae981a02c2741736e1f130030635c98268b34
turnerem <turnerem@tcd.ie>
2022-09-21 18:46:14 +0100

    initial commit

---
+++ blogposts.go
@@ -0,0 +1,14 @@
+package blogposts
+
+import (
+  "testing/fstest"
+)
+
+// Post is...
+type Post struct {
+}
+
+// NewPostsFromFS is...
+func NewPostsFromFS(fileSystem fstest.MapFS) []Post {
+  return nil
+}
